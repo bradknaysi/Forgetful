ab15bbb94f897d502c0867f6d5398306c3972a6d
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2017-12-24 17:57:14 -0500

    Initial Commit

---
+++ Forgetful/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forgetful
+//
+//  Created by Bradley Knaysi on 12/24/17.
+//  Copyright © 2017 Bradley Knaysi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
